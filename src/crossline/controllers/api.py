36d1c8caea2c44fb6fe941de0e4a9558c034cd02
joamag <joamag@gmail.com>
2013-11-11 19:29:56 +0000

    initial crossline support

--- src/crossline/controllers/api.py
+++ src/crossline/controllers/api.py
@@ -11,23 +11,42 @@ class ApiController(appier.Controller, appier.Mongo):
         appier.Controller.__init__(self, owner, *args, **kwargs)
         appier.Mongo.__init__(self, *args, **kwargs)
 
-    @appier.route("/api/cross", ("GET", "POST"))
-    def cross(self):
+    @appier.route("/api/cross", "GET")
+    @appier.route("/api/<app>/cross", "GET")
+    def cross(self, app = None):
         db = self.get_db("crossline")
 
         current = datetime.datetime.utcnow()
 
         filter = dict(
+            app = app,
             year = current.year,
             month = current.month,
             day = current.day,
             hour = current.hour
         )
 
-        fact = db.crossline.find_one(filter) or filter
+        fact = db.facts.find_one(filter) or filter
         count = fact.get("count", 0) + 1
         fact["count"] = count
 
-        db.crossline.save(fact)
+        db.facts.save(fact)
 
         return count
+
+    @appier.route("/api/facts", "GET")
+    @appier.route("/api/<app>/facts", "GET")
+    def facts(self, app = None):
+        db = self.get_db("crossline")
+
+        filter = dict(
+            app = app
+        )
+
+        cursor = db.facts.find(filter)
+        facts = [fact for fact in cursor]
+        for fact in facts: del fact["_id"]
+
+        return dict(
+            facts = facts
+        )
--- src/crossline/controllers/web.py
+++ src/crossline/controllers/web.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import appier
+import datetime
 
 class WebController(appier.Controller, appier.Mongo):
 
@@ -9,3 +10,26 @@ class WebController(appier.Controller, appier.Mongo):
     def __init__(self, owner, *args, **kwargs):
         appier.Controller.__init__(self, owner, *args, **kwargs)
         appier.Mongo.__init__(self, *args, **kwargs)
+
+    @appier.route("/cross", "GET")
+    @appier.route("/<app>/cross", "GET")
+    def cross(self, app = None):
+        db = self.get_db("crossline")
+
+        current = datetime.datetime.utcnow()
+
+        filter = dict(
+            app = app,
+            year = current.year,
+            month = current.month,
+            day = current.day,
+            hour = current.hour
+        )
+
+        fact = db.facts.find_one(filter) or filter
+        count = fact.get("count", 0) + 1
+        fact["count"] = count
+
+        db.facts.save(fact)
+
+        return count
